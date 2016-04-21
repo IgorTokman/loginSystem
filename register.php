9c69954634063862af623e1c7ec5a7d8e6371c69
IgorTokman <igortokman1994@ukr.net>
2016-04-21 12:57:31 +0300

    added the registration form. created the Input class

---
+++ register.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'core/init.php';
+
+if(Input::exists())
+    
+?>
+
+<form action="" method="POST">
+
+    <div class="field">
+        <label for="username">Username</label>
+        <input type="text" name="username" id="username"
+            value="" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Choose a password</label>
+        <input type="password" name="password" id="password">
+    </div>
+
+    <div class="field">
+        <label for="password_again">Enter your password again</label>
+        <input type="password" name="password_again" id="password_again">
+    </div>
+
+    <div class="field">
+        <label for="name">Some field</label>
+        <input type="text" name="name" id="name">
+    </div>
+
+    <input type="submit" value="Register" >
+</form>
